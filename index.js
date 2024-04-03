0155c919681453a222a86da675f597e40170b078
Vincent <vincentnjogu611@gmail.com>
2024-04-03 16:09:50 +0300

    array find method labs

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(superbowls) {
+    const winningYear = superbowls.find(superbowl => superbowl.result === "W");
+    return winningYear ? winningYear.year : undefined;
+}
